b768024a711b579703611fc06d973fae5b0b001b
AdCoder <bikeev1999@mail.ru>
2020-09-20 12:43:33 +0300

    BACK-1
    Добавил класс для описания места в холодильнике

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Commander
+namespace Refrigerator
 {
     public class Startup
     {
