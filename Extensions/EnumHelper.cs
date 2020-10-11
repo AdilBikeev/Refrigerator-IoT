08e7d4718efda6fd8895338b3ad2a488115fc366
AdCoder <bikeev1999@mail.ru>
2020-10-12 02:54:32 +0300

    BACK-2 Добавли Mock для сенсоров

---
+++ Extensions/EnumHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Extensions
+{
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        /// <example>string desc = myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;</example>
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+
+        // This method creates a specific call to the above method, requesting the
+        // Description MetaData attribute.
+        public static string ToName(this Enum value)
+        {
+            var attribute = value.GetAttributeOfType<DescriptionAttribute>();
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
---
+++ SimulatedDevice/Models/BaseSensorData.cs
@@ -0,0 +1,15 @@
+﻿using RefrigeratorRemote.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulatedDevice.Models
+{
+    public abstract class BaseSensorData
+    {
+        /// <summary>
+        /// Объект сенсора на блоке холодильника.
+        /// </summary>
+        public SensorData SensorData { get; protected set; }
+    }
+}
