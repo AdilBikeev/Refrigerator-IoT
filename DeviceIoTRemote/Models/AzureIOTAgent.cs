23c9cd0f7138890c6cbc1401437963f09d019b5f
AdCoder <bikeev1999@mail.ru>
2020-10-12 04:35:58 +0300

    BACK-2 Добавил метод для чтения данных из реестра Azure IoT

--- DeviceIoTRemote/Models/AzureIOTAgent.cs
+++ DeviceIoTRemote/Models/AzureIOTAgent.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Azure.Devices.Client;
+using Azure.Messaging.EventHubs.Consumer;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RemoteProvider.Models
@@ -23,6 +25,18 @@ namespace RemoteProvider.Models
         // Using the Azure CLI:
         // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
         private readonly string connectionString = "HostName=Refrigerator.azure-devices.net;DeviceId=RefrigeratorDevice;SharedAccessKey=crmWehLrWmeDBFp9TyXLLoVWJoMbJBxvPtbj7Vw3mA4=";
+
+        // Event Hub-compatible endpoint
+        // az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+        private const string EventHubsCompatibleEndpoint = "sb://iothub-ns-refrigerat-5009613-1d5e48ae28.servicebus.windows.net/";
+
+        // Event Hub-compatible name
+        // az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+        private const string EventHubName = "refrigerator";
+
+        // az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+        private const string IotHubSasKeyName = "service";
+        private const string IotHubSasKey = "SeST5mFbdK3284heuL1Tkzi7b35WbGzieuWJDXohcBA=";
 
         public AzureIOTAgent()
         {
@@ -55,6 +69,42 @@ namespace RemoteProvider.Models
                 await Task.Delay(perSeconds*1000);
 
                 sensorData.Update();
+            }
+        }
+
+        private static string BuildEventHubsConnectionString(string eventHubsEndpoint,
+                                                     string iotHubSharedKeyName,
+                                                     string iotHubSharedKey) =>
+    $"Endpoint={ eventHubsEndpoint };SharedAccessKeyName={ iotHubSharedKeyName };SharedAccessKey={ iotHubSharedKey }";
+
+        /// <summary>
+        /// Метод для чтения сообщения из реестра Azure IoT.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task ReceiveMessagesFromDeviceAsync(CancellationToken cancellationToken)
+        {
+            string connectionString = BuildEventHubsConnectionString(EventHubsCompatibleEndpoint, IotHubSasKeyName, IotHubSasKey);
+
+            await using EventHubConsumerClient consumer = new EventHubConsumerClient(EventHubConsumerClient.DefaultConsumerGroupName, connectionString, EventHubName);
+
+            Console.WriteLine("Listening for messages on all partitions");
+
+            try
+            {
+                await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync(cancellationToken))
+                {
+                    Console.WriteLine("Message received on partition {0}:", partitionEvent.Partition.PartitionId);
+
+                    // Переданное IoT устройством сообщение
+                    string data = Encoding.UTF8.GetString(partitionEvent.Data.Body.ToArray());
+                    Console.WriteLine("\t{0}:", data);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // This is expected when the token is signaled; it should not be considered an
+                // error in this scenario.
             }
         }
     }
