70e39c22a59c1848fbd90f342112b7696e683a7b
AdCoder <bikeev1999@mail.ru>
2020-10-13 18:48:49 +0300

    BACK-3 Добавил метод для конвертации object в XmlDocument

---
+++ DeviceIoTRemote/Models/BaseRemoteClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RemoteProvider.Models
+{
+    public abstract class BaseRemoteClient : IConverterData
+    {
+        public XmlDocument ConvertData()
+        {
+            using (var stringwriter = new System.IO.StringWriter())
+            {
+                var serializer = new XmlSerializer(this.GetType());
+                serializer.Serialize(stringwriter, this);
+
+                var xmlData = new XmlDocument();
+                xmlData.LoadXml(stringwriter.ToString());
+
+                return xmlData;
+            }
+        }
+    }
+}
---
+++ DeviceIoTRemote/Models/IConverterData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RemoteProvider.Models
+{
+    public interface IConverterData
+    {
+        /// <summary>
+        /// Возвращает данные в формате <see cref="XmlDocument"/>.
+        /// </summary>
+        public XmlDocument ConvertData(); 
+    }
+}
